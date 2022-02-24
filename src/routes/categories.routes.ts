63d8c1fa4985867f9980e14f598d4eb3c825c62b
Ricardo <ricardoinliner@yahoo.com>
2022-02-24 11:03:03 -0300

    refactor: organizing using modules
    feat: cars module created

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from "express";
 
-import { CategoryRepository } from "../repositories/CategoryRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { CategoryRepository } from "../modules/cars/repositories/CategoryRepository";
+import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 
